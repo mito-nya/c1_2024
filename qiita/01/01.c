53529c9f7b9a19ae73089b3cd59689f125aec565
mito_nya <ohrtty7@gmail.com>
2024-04-12 01:20:23 +0900

    01

--- qiita/01/01.c
+++ qiita/01/01.c
@@ -17,6 +17,16 @@ void matprd(float *xpt, float *ypt, float *zpt, int n, int l, int m){
     }
 }
 
+void output(float *zpt, int n, int m){
+    int i, j, k;
+    for (i = 0; i < n; i++){
+        for (j=0; j<m; j++){
+            printf("%10.2f", *(zpt+i*m+j));
+        }
+        printf("\n");
+    }
+}
+
 int main(void){
     static float a[N][L]={{1., 2., 3.}, {4., 5., 6.}, {7., 8., 9.}};
     static float b[L][M]={{10., 11., 12.}, {13., 14., 15.}, {16., 17., 18.}};
@@ -26,11 +36,12 @@ int main(void){
 
     matprd(&a[0][0], &b[0][0], &c[0][0], N, L, M);
 
-    for (i=0; i<N; i++){
-        for (j=0; j<M; j++){
-            printf("%10.2f", c[i][j]);
-        }
-        printf("\n");
-    }
+    // for (i=0; i<N; i++){
+    //     for (j=0; j<M; j++){
+    //         printf("%10.2f", c[i][j]);
+    //     }
+    //     printf("\n");
+    // }
+    output(&c[0][0], N, M);
     return 0;
 }
