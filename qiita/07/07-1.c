54bdbacb4dad916dd7bbbfbf19ab6e477ce3281d
mito_nya <ohrtty7@gmail.com>
2024-04-13 18:43:41 +0900

    07

--- qiita/07/07-1.c
+++ qiita/07/07-1.c
@@ -2,7 +2,7 @@
 #include <math.h>
 
 double f(double x, double y){
-    return -y;
+    return -x*y+y*y;
 }
 
 void Euler(double x, double *y, double h, double funcpt(double x, double y)){
@@ -13,7 +13,7 @@ int main(void){
     double x, y, h;
     int i, imax;
     
-    x=0.;
+    x=1.;
     y=1.;
     h=1e-2;
     imax=1./h+1;
@@ -21,7 +21,7 @@ int main(void){
     for(i=0;i<imax;i++){
         Euler(x, &y, h, f);
         x=x+h;
-        printf("%d %e %e\n", i, x, y);
+        printf("%d, %e, %e\n", i, x, y);
     }
     return 0;
 }
