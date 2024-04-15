810999c04d96622942d311d5b98bcd2206841613
mito_nya <ohrtty7@gmail.com>
2024-04-15 15:54:15 +0900

    10

---
+++ qiita/10/10-3.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#define PI 3.14159265
+#define E 2.718281828
+
+double f(double x){
+    return sin(x);
+}
+
+double daikei(double h, double a, double b, double *I, double funcpt(double x)){
+    int N=(b-a)/h;
+    for(int i=0;i<N;i++){
+        *I=*I+(funcpt(i*h)+funcpt((i+1)*h))*h*.5;
+    }
+}
+
+double simpson(double h, double a, double b, double *J, double funcpt(double x)){
+    int N=(b-a)/h;
+    for(int i=0;i<N;i++){
+        *J=*J+(funcpt(i*h)+4.*funcpt((i+.5)*h)+funcpt((i+1)*h))*h/6.;
+    }
+}
+
+int main(void){
+    double h=1.e-2;
+    double a=0.;
+    double b=2.*PI;
+    double I, J;
+    daikei(h, a, b, &I, f);
+    simpson(h, a, b, &J, f);
+    printf("value  : %e\ndaikei : %e\nsimpson: %e", 0., I, J);
+    return 0;
+}
